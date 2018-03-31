eeb2921e376226846bf51fcc50e7b7dbeb69d1c7
davidkamedulski <37964590+davidkamedulski@users.noreply.github.com>
2018-03-31 15:46:18 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am Rich 2
+//
+//  Created by David Kamedulski1 on 3/30/18.
+//  Copyright © 2018 David Kamedulski1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
